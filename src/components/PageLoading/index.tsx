746d3931f2ca0c1d9e63aef356991e7569daed9b
zzx0106 <1148218318@qq.com>
2022-10-09 14:42:11 +0800

    Update index.tsx

--- src/components/PageLoading/index.tsx
+++ src/components/PageLoading/index.tsx
@@ -1,9 +1,3 @@
-/*
- * @Author: linzhihai
- * @Date: 2021-09-03 14:25:44
- * @LastEditTime: 2021-09-03 14:27:59
- * @Description: 组件Loading
- */
 import { CSSModule } from '@/utils/css-module';
 import { Spin } from 'antd';
 import styles from './index.module.scss';
